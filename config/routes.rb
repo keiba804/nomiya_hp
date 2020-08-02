d1608a2674ded636841ccfc90e041af2ed978f79
keiba804 <mottuso0017@gmail.com>
2020-08-02 10:36:46 +0000

    edit config/routes,views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :restaurants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :restaurants
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :drink_categories, only: [:index, :create, :update, :destroy]
+	resources :food_categories, only: [:index, :create, :update, :destroy]
+  	resources :drink_menus, only: [:index, :create, :edit, :update, :destroy, :new]
+  	resources :food_menus, only: [:index, :create, :edit, :update, :destroy, :new]
+  	resources :informations
+  	resources :restaurants, only: [:show, :update, :edit]
+  	resources :restaurant_images
+
 end
